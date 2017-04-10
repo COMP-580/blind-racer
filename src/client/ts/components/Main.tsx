5e6a2260782821f54b98a22c94f1ab4b413370fd
tzhu65 <tzhu65@gmail.com>
2017-04-10 19:12:41 -0400

    remove banner component

--- src/client/ts/components/Main.tsx
+++ src/client/ts/components/Main.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 
-import Banner from "./Banner";
 import SpeedDisplayer from "./Timer/SpeedDisplayer";
 import ViewBox from "./ViewBox";
 import TextInput from "./WordInput/TextInput";
@@ -9,8 +8,6 @@ class Main extends React.Component<undefined, undefined> {
   public render() {
     return (
       <div>
-        <h1>Test</h1>
-        <Banner />
         <ViewBox id="view-box" />
         <TextInput id="text-input" defaultValue="" />
         <SpeedDisplayer />
