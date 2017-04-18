ffaeac5613e23c787c4d5ab04fcd1ee488f76b32
tzhu65 <tzhu65@gmail.com>
2017-04-18 16:46:39 -0400

    added a rest client

--- src/client/ts/util/RestClient.ts
+++ src/client/ts/util/RestClient.ts
@@ -6,6 +6,54 @@
 
 class RestClient {
 
+  public get(url: string, success: (data: any, status: any) => void, fail: (msg?: string) => void): void {
+
+    $.get(url, (data, status) => {
+      if (status === "success") {
+        success(data, status);
+      } else {
+        if (fail) {
+          fail();
+        }
+      }
+    }).fail((res, status) => {
+      let msg: string;
+      if (res.responseJSON && res.responseJSON.error && res.responseJSON.error.message) {
+        msg = res.responseJSON.error.message;
+      } else {
+        msg = res.status + " " + res.statusText;
+      }
+      if (fail) {
+        fail(msg);
+      }
+    });
+  }
+
+  public postMessage(url: string, postData: any,
+                     success: (data: any, status: any) => void,
+                     fail: (msg?: string) => void) {
+
+    $.post(url, postData, (data, status) => {
+      if (status === "success") {
+        success(data, status);
+      } else {
+        if (fail) {
+          fail();
+        }
+      }
+    }).fail((res, status) => {
+      let msg: string;
+      if (res.responseJSON && res.responseJSON.error && res.responseJSON.error.message) {
+        msg = res.responseJSON.error.message;
+      } else {
+        msg = res.status + " " + res.statusText;
+      }
+      if (fail) {
+        fail(msg);
+      }
+    });
+  }
+
 }
 
 let restClient = new RestClient();
