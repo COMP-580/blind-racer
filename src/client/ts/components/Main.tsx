1735f293c798c3fd1a52b9cd19a1031ebebf99e1
tzhu65 <tzhu65@gmail.com>
2017-04-19 16:26:29 -0400

    basic layout

--- src/client/ts/components/Main.tsx
+++ src/client/ts/components/Main.tsx
@@ -7,7 +7,9 @@
 import * as React from "react";
 
 import GameText from "./GameText/GameText";
+import Leaderboard from "./Leaderboard/Leaderboard";
 import NavBar from "./NavBar/NavBar";
+import Title from "./Title/Title";
 import UserInput from "./UserInput/UserInput";
 import UserStats from "./UserStats/UserStats";
 import VirtualKeyboard from "./VirtualKeyboard/VirtualKeyboard";
@@ -20,21 +22,41 @@ import SettingsModal from "./Modals/Settings/SettingsModal";
 class Main extends React.Component<undefined, undefined> {
   public render() {
     return (
-      <div className="container-fluid">
+      <div className="container">
 
-        <NavBar />
+        <div className="row vertical-center">
 
-        <div>
-          <GameText id="view-box" />
-          <UserInput id="text-input" defaultValue="" />
-          <UserStats />
-          <VirtualKeyboard />
-          <NavBar />
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="row">
+              <Title />
+            </div>
+
+            <div className="row row-same-height">
+              <div className="col-xs-3 force-column-height">
+                <NavBar />
+              </div>
+
+              <div className="col-xs-6 force-column-height">
+                <GameText id="view-box" />
+                <UserInput id="text-input" defaultValue="" />
+                <UserStats />
+              </div>
+
+              <div className="col-xs-3 force-clumn-height">
+                <Leaderboard />
+              </div>
+
+              <div className="col-xs-12">
+                <VirtualKeyboard />
+              </div>
+            </div>
+          </div>
+
         </div>
 
-        <AboutModal />
-        <HelpModal />
-        <SettingsModal />
+          <AboutModal />
+          <HelpModal />
+          <SettingsModal />
 
       </div>
     );
---
+++ src/client/ts/components/Title/Title.tsx
@@ -0,0 +1,24 @@
+/**
+ * Title
+ *
+ * Title for the center display.
+ */
+
+import * as React from "react";
+
+class Title extends React.Component<any, any> {
+
+  constructor() {
+    super();
+  }
+
+  public render() {
+    return (
+      <div>
+        <h4>Title</h4>
+      </div>
+    );
+  }
+}
+
+export default Title;
